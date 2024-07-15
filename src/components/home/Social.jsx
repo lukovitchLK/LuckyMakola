ff38545f2abf4b3cc15eeff7812fd36f67fdce56
Lucky Makola <luckymakola@Luckys-MacBook-Air.local>
2024-07-15 14:16:16 +0200

    first commit

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+const Social = () => {
+  return (
+    <div className="home__social">
+      <a
+        href="https://instagram.com/"
+        className="home__social-icon"
+        target="_blank"
+      >
+        <i className="uil uil-instagram"></i>
+      </a>
+      <a
+        href="https://instagram.com/"
+        className="home__social-icon"
+        target="_blank"
+      >
+        <i className="uil uil-dribble"></i>
+      </a>
+      <a
+        href="https://instagram.com/"
+        className="home__social-icon"
+        target="_blank"
+      >
+        <i className="uil uil-github-alt"></i>
+      </a>
+    </div>
+  );
+};
+
+export default Social;
---
+++ src/components/services/Services.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./services.css";
+
+const Services = () => {
+  return (
+    <section className="services section" id="services">
+      <h2 className="section__title">Services</h2>
+      <span className="section__subtitle">What I offer</span>
+    </section>
+  );
+};
+
+export default Services;
